03e12b33e5eb6f8d6f461523b9354adf40e56647
storm-crypto <1742593818@qq.com>
2023-03-28 14:48:52 +0800

    Scanner读入

--- src/main/java/com/ljx/Main.java
+++ src/main/java/com/ljx/Main.java
@@ -1,13 +1,11 @@
 package com.ljx;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        char c = 'A';
-        int x = (int)c;
-        int y = 12;
-        double z = y; // 隐式类型转化只能从低到高变化
-        System.out.println(x);
-        System.out.println(z);
-
+        Scanner sc = new Scanner(System.in);
+        String str = sc.next();
+        System.out.println(str);
     }
 }
