2d0d9c792948a24d1036ce4b638dd7dd84ddf1ec
storm-crypto <1742593818@qq.com>
2023-03-28 15:26:49 +0800

    多维数组 常用API： 数组的属性length 数组排序Arrays.sort 数组赋值Arrays.fill Arrays.toString 输出一维数组 Arrays.deepToString：输出多维数组的所有值

--- src/main/java/com/ljx/Main.java
+++ src/main/java/com/ljx/Main.java
@@ -5,16 +5,49 @@ import com.sun.javaws.IconUtil;
 import java.awt.*;
 import java.io.*;
 import java.nio.BufferOverflowException;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args){
-        int[] a = new int[5]; // 数组初始化
-        int n = 10;
-        float[] b = new float[n];
-        char[] c = {'a', 'b', 'c'};
-        char[] d = c;
-        d[1] = 'x';
-        System.out.println(c[1]);
+//        // 数组的输入和输出
+//        int[] a = new int[5];
+//        for (int i = 0; i < 5; i++) {
+//            a[i] = i;
+//        }
+//
+//        for (int i = 0; i < 5; i++) {
+//            System.out.println(a[i] * a[i]);
+//        }
+
+//        // 二维数组
+//        int[][] a = new int[2][3];
+//        a[1][2] = 3;
+//
+//        int[][] b = {
+//                {1, 2, 3},
+//                {4, 5, 6}
+//        };
+//
+//        System.out.println(Arrays.deepToString(a));
+//        System.out.println(Arrays.deepToString(b));
+
+//        // 数组的属性length
+//        int[] a = new int[10];
+//
+//        for (int i = 0; i < a.length; i++) {
+//            a[i] = 10;
+//        }
+
+//        // 数组的排序 sort
+//        int[] a = {2, 1, 5, 3, 4};
+//        Arrays.sort(a);
+//        System.out.println(Arrays.toString(a));
+
+        // 数组赋值fill
+        int[] a = {2, 1, 5, 3, 4};
+
+        Arrays.fill(a, 3);
+        System.out.println(Arrays.toString(a));
     }
 }
