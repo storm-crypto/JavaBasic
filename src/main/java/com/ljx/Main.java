f50dc296244145d335a0403908308c9c5adbd727
storm-crypto <1742593818@qq.com>
2023-03-28 14:51:42 +0800

    BufferedReader读入数据

--- src/main/java/com/ljx/Main.java
+++ src/main/java/com/ljx/Main.java
@@ -1,11 +1,16 @@
 package com.ljx;
 
+import java.awt.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.StringReader;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        String str = sc.next();
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String str = br.readLine();
         System.out.println(str);
     }
 }
