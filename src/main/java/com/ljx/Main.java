759cafdd63e1de97abf73ebceb7508b3e28f46e5
storm-crypto <1742593818@qq.com>
2023-03-28 14:46:08 +0800

    隐式类型转化

--- src/main/java/com/ljx/Main.java
+++ src/main/java/com/ljx/Main.java
@@ -2,6 +2,12 @@ package com.ljx;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        char c = 'A';
+        int x = (int)c;
+        int y = 12;
+        double z = y; // 隐式类型转化只能从低到高变化
+        System.out.println(x);
+        System.out.println(z);
+
     }
 }
