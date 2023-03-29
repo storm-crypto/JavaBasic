a0ac915ea1373bb81e8ef22d01956851fdf5ad56
storm-crypto <1742593818@qq.com>
2023-03-29 19:42:17 +0800

    Set也是Interface类型的，这里举了TreeSet(有序集合)

--- src/main/java/com/ljx/Main.java
+++ src/main/java/com/ljx/Main.java
@@ -5,12 +5,12 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args){
-        Set<Integer> set  = new HashSet<>(); // 无序
+        TreeSet<Integer> set  = new TreeSet<>(); // 无序
 
         set.add(1);
         set.add(2);
         set.add(3);
 
-        System.out.println(set.contains(3));
+        System.out.println(set.ceiling(4));
     }
 }
