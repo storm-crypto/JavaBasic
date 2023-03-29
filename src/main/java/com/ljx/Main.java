5dcc3adff5e6d38b5b23db47b7953f26809527c5
storm-crypto <1742593818@qq.com>
2023-03-29 19:32:44 +0800

    List容器

--- src/main/java/com/ljx/Main.java
+++ src/main/java/com/ljx/Main.java
@@ -1,65 +1,18 @@
 package com.ljx;
 
-import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.List;
 
-interface Role{
-    public void greet();
-    public void move();
-    public int getSpeed();
-}
 
-// 接口的继承
-interface Hero extends Role{
-    public void attack();
-}
-
-class Zeus implements Hero, Role{
-    private final String name = "Zeus";
-    public void greet(){
-        System.out.println(name + ":Hi!" );
-    }
-
-    public void move(){
-        System.out.println(name + ":Move!" );
-    }
-
-    public int getSpeed(){
-        return 10;
-    }
-
-    public void attack(){
-        System.out.println(name + ": Attack!");
-    }
-}
-
-class Athena implements Hero{
-    private final String name = "Athena";
-    public void greet(){
-        System.out.println(name + ":Hi!" );
-    }
-
-    public void move(){
-        System.out.println(name + ":Move!" );
-    }
-
-    public int getSpeed(){
-        return 10;
-    }
-
-    public void attack(){
-        System.out.println(name + ": Attack!");
-    }
-}
 
 public class Main {
     public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
-        System.out.println("请选择英雄：");
-        String name = sc.next();
-        Hero hero;
-        if (name.equals("Zeus")) hero = new Zeus();
-        else hero = new Athena();
+       List<Integer> list = new ArrayList<>();
 
-        hero.greet();
+       list.add(1);
+       list.add(2);
+       list.set(0, 100);
+
+        System.out.println(list.get(0));
     }
 }
