7c43a877c96d47d22b87709335060e47c6aaf541
storm-crypto <1742593818@qq.com>
2023-03-29 17:10:31 +0800

    类的继承

--- src/main/java/com/ljx/Main.java
+++ src/main/java/com/ljx/Main.java
@@ -9,7 +9,7 @@ import java.util.Arrays;
 import java.util.Scanner;
 
 class Point{
-    private int x;
+    private int x; // 私有变量在子类中也不能被访问
     private int y;
 
     public Point(int x){
@@ -42,12 +42,32 @@ class Point{
     }
 }
 
+class ColorPoint extends Point{
+    private String color;
+
+    public ColorPoint(int x, int y, String color){
+        super(x, y); // 父类
+        this.color = color;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    // 重写父类函数
+    public String toString(){
+        return String.format("(%d, %d, %s)", super.getX(), super.getY(), color);
+    }
+}
+
 public class Main {
     public static void main(String[] args){
-        Point point = new Point(3, 4);
-
-        point.setX(5);
-
-        System.out.println(point.toString());
+        ColorPoint colorPoint = new ColorPoint(3, 4, "red");
+        System.out.println(colorPoint.toString());
+        System.out.println(colorPoint.getX()); // 访问符类的非private的成员变量和成员函数
     }
 }
